1e9cca7613346792fa5103c9fb36efe5c2bf640d
Jeff Church <jchurch2221@gmail.com>
2022-11-25 08:38:27 -0600

    update

--- Accessors/UpdateDB.cs
+++ Accessors/UpdateDB.cs
@@ -1,11 +1,33 @@
-﻿namespace Accessors
-{
-    public class CreateCustomer
-    {
+﻿
 
+using System.Data.SqlClient;
+using Tools;
+using DataContainers;
+
+namespace Accessors
+{
+    public interface ICreateCustomer
+    {
+        public void Create(Customer customer);
+    }
+
+    public class CreateCustomer : ICreateCustomer
+    {
+        public void Create(Customer customer)
+        {
+            using (SqlConnection con = DBTools.ConnectToDB())
+                using (SqlCommand cmd = con.CreateCommand())
+            {
+
+            }
+        }
     }
 
     public class UpdateCart
     {
+        public void Update()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
---
+++ Tools/DBTools.cs
@@ -0,0 +1,14 @@
+﻿using System.Data.SqlClient;
+
+namespace Tools
+{
+    public class DBTools
+    {
+        public static SqlConnection ConnectToDB()
+        {
+            SqlConnection con = new("Data Source=localhost\\SQLEXPRESS;Initial Catalog=Group5;Integrated Security=True");
+            return con;
+        }
+
+    }
+}
