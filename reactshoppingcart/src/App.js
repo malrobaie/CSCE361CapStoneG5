c36ff6be363fc8b695d5d32a63755a1de314c51f
Mohammad Alrobaie <alrobaiemohammad@gmail.com>
2022-11-26 01:10:41 -0600

    React Nav and banner

--- reactshoppingcart/src/App.js
+++ reactshoppingcart/src/App.js
@@ -1,11 +1,13 @@
 import React, { useState } from 'react';
 import './App.css';
 import Navbar from './Components/Navbar'
+import Home from './Components/Home';
 
 export default function App() {
     return (
         <>
         <Navbar />
+        <Home />
         </>
     );
 }
--- reactshoppingcart/src/Components/Navbar.jsx
+++ reactshoppingcart/src/Components/Navbar.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 export default function Navbar() {
     return (
         <div>
-            <nav class="navbar navbar-expand-lg bg-light">
+            <nav class="navbar navbar-expand-lg">
                 <div class="container">
-                    <a class="navbar-brand fw-bold fs-4" href="#"><span>e</span>cart</a>
+                    <a class="navbar-brand fw-bold fs-4" href="#"><img src={require("../assets/logo.png")} height='34px'/></a>
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
                     </button>
@@ -26,16 +26,15 @@ export default function Navbar() {
 
                         </ul>
                         <form class="d-flex" role="search">
-                            <input class="form-control me-2" type="search" placeholder="Search for anything..." aria-label="Search" />
-                            <button class="btn btn-outline-success" type="submit">Search</button>
+                            <input class="form-control me-2 bg-light text-dark border-0" type="search" placeholder="Search for anything..." aria-label="Search" />
+                            <button class="btn btn-clear" type="submit"><img src={require('../assets/search.png')} height='30'/></button>
                         </form>
                         <div className="buttons">
-                            <a href="" className="btn btn-outline-dark ms-2">
-                                <i className="fa fa sign-in me-1"></i> Login</a>
-                            <a href="" className="btn btn-outline-dark ms-2">
-                                <i className="fa fa sign-in me-1"></i> Register</a>
-                            <a href="" className="btn btn-outline-dark ms-2">
-                                <i className="fa fa sign-in ms-1"></i> Cart (0)</a>
+                            <a href="" className="btn btn-clear ms-2">
+                                <i className="login"></i><img src={require("../assets/login.png")} height='33px'/></a>
+                            
+                            <a href="" className="btn btn-clear ms-2">
+                                <i className="cart"></i><img src={require("../assets/cart.png")} height='33px'/></a>
                         </div>
                     </div>
                 </div>
