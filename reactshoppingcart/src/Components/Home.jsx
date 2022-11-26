99f1daa168f5658a9f376973f9debb3fdcd32f9c
Mohammad Alrobaie <alrobaiemohammad@gmail.com>
2022-11-26 03:06:10 -0600

    React carousel

--- reactshoppingcart/src/Components/Home.jsx
+++ reactshoppingcart/src/Components/Home.jsx
@@ -1,32 +1,43 @@
 import React from 'react'
+import Carousel from 'react-bootstrap/Carousel';
 
-export default function Home() {
-    return (
-        <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="true">
-  <div class="carousel-indicators">
-    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
-    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
-  </div>
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-      <img src={require("../assets/banner1.png")} class="d-block w-100" alt="..."/>
-    </div>
-    <div class="carousel-item">
-      <img src={require("../assets/banner2.png")} class="d-block w-100" alt="..."/>
-    </div>
-    <div class="carousel-item">
-      <img src={require("../assets/banner3.png")} class="d-block w-100" alt="..."/>
-    </div>
-  </div>
-  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Previous</span>
-  </button>
-  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Next</span>
-  </button>
-</div>
-    )
+function IndividualIntervalsExample() {
+  return (
+    <Carousel>
+      <Carousel.Item interval={1000}>
+        <img
+          className="d-block w-100"
+          src={require("../assets/banner1.png")}          alt="First slide"
+        />
+        <Carousel.Caption>
+          {/* <h3>First slide label</h3>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p> */}
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item interval={1000}>
+        <img
+          className="d-block w-100"
+          src={require("../assets/banner2.png")}          alt="Second slide"
+        />
+        <Carousel.Caption>
+          {/* <h3>Second slide label</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p> */}
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src={require("../assets/banner3.png")}          alt="Third slide"
+        />
+        <Carousel.Caption>
+          {/* <h3>Third slide label</h3>
+          <p>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          </p> */}
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+  );
 }
+
+export default IndividualIntervalsExample;
