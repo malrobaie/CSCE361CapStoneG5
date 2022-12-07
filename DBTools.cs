4ca55d852d53f26a49aba7f88260b83b35fe5105
kakerberg <69698401+kakerberg@users.noreply.github.com>
2022-12-07 13:22:05 -0600

    Verified database connectivity, added password row in customer table

---
+++ DBTools.cs
@@ -0,0 +1,16 @@
+﻿using DataContainers;
+using Microsoft.AspNetCore.Hosting.Server;
+using System.ComponentModel.DataAnnotations;
+using System.Data.SqlClient;
+
+namespace Tools
+{
+    public class DBTools
+    {
+        public static SqlConnection ConnectToDB()
+        {
+            SqlConnection con = new("Server=localhost; Database=CSCE361G5; User Id=sa; Password=Csce361G5sql#");
+            return con;
+        }
+    }
+}
