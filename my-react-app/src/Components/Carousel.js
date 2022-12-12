002aae689e339b400576a5d011703d3cfd4d80b2
malrobaie <112534261+malrobaie@users.noreply.github.com>
2022-12-11 18:08:47 -0600

    Update Carousel.js

--- my-react-app/src/Components/Carousel.js
+++ my-react-app/src/Components/Carousel.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Carousel from 'react-bootstrap/Carousel'
 
+// carousel promo banner
 function adCarousel() {
     return (
         <Carousel>
@@ -10,8 +11,6 @@ function adCarousel() {
                     src={require("../assets/banner1.png")} alt="First slide"
                 />
                 <Carousel.Caption>
-                    {/* <h3>First slide label</h3>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p> */}
                 </Carousel.Caption>
             </Carousel.Item>
             <Carousel.Item interval={1000}>
@@ -20,8 +19,6 @@ function adCarousel() {
                     src={require("../assets/banner2.png")} alt="Second slide"
                 />
                 <Carousel.Caption>
-                    {/* <h3>Second slide label</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p> */}
                 </Carousel.Caption>
             </Carousel.Item>
             <Carousel.Item>
@@ -30,10 +27,6 @@ function adCarousel() {
                     src={require("../assets/banner3.png")} alt="Third slide"
                 />
                 <Carousel.Caption>
-                    {/* <h3>Third slide label</h3>
-          <p>
-            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-          </p> */}
                 </Carousel.Caption>
             </Carousel.Item>
         </Carousel>
