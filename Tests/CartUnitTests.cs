ab481a8a7c22c06d7046f85a4fa8929d4f80e774
kevin <kakerberg2@huskers.unl.edu>
2022-12-12 17:12:18 -0600

    Removed old test folder, added some engine functionality and wrote some cart unit tests

--- Tests/CartUnitTests.cs
+++ Tests/CartUnitTests.cs
@@ -1,4 +1,7 @@
 ﻿using DataContainers;
+using Engine;
+using Accessors.PullFromDB;
+
 namespace Tests
 {
 	[TestClass]
@@ -7,8 +10,43 @@ namespace Tests
 		[TestMethod]
 		public void TestCartTotal1()
 		{
-			//var cart = new Cart(); // assumes we have a data container for a cart
-			//cart.add(); // need methods for the cart to add and take away items from its list of products. Mohammad was working on this in the front end, but it seems like more of a back end thing
+			var newCust = new GetCustomer();
+			Customer customer = newCust.GetCustomerFromEmail("tshrubsall2@sitemeter.com");
+			GetCart.GetCustomerCart(customer);
+
+			var cartTools = new CartTools();
+			double total = cartTools.getCartTotal("tshrubsall2@sitemeter.com");
+			double expected = 2524.95;
+
+			Assert.Equals(total, expected);
 		}
-	}
+
+        [TestMethod]
+        public void TestCartTotal2()
+        {
+            var newCust = new GetCustomer();
+            Customer customer = newCust.GetCustomerFromEmail("mblakeston@businessinsider.com");
+            GetCart.GetCustomerCart(customer);
+
+            var cartTools = new CartTools();
+            double total = cartTools.getCartTotal("mblakeston@businessinsider.com");
+            double expected = 374.98;
+
+            Assert.Equals(total, expected);
+        }
+
+        [TestMethod]
+        public void TestCartTotal3()
+        {
+            var newCust = new GetCustomer();
+            Customer customer = newCust.GetCustomerFromEmail("dellenbrook3@yahoo.com");
+            GetCart.GetCustomerCart(customer);
+
+            var cartTools = new CartTools();
+            double total = cartTools.getCartTotal("dellenbrook3@yahoo.com");
+            double expected = 369.99;
+
+            Assert.Equals(total, expected);
+        }
+    }
 }
